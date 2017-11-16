e41fa7716070820f05ae6aa12d69983b4cb62615
johanjerger <ocamposjuancruz23@gmail.com>
2017-11-16 11:37:48 -0300

    UVa 10038 solved

--- UVa Online Judge/10000/10038_jolly_jumpers.cpp
+++ UVa Online Judge/10000/10038_jolly_jumpers.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
 int main()
 {
-  int numbers, diffs[3005];
-  bool not_jolly;
+  int numbers, seq[3005], diffs[3005];
+  set <int> differences;
+  bool is_jolly;
 
   while (cin >> numbers) {
-      not_jolly = true;
-      cin >> diffs[0];
+      is_jolly = true;
+      cin >> seq[0];
       for (int i=1; i < numbers; i++) {
-        cin >> diffs[i];
-        diffs[i-1] = abs(diffs[i-1] - diffs[i]);
+        cin >> seq[i];
+        diffs[i-1] = abs(seq[i] - seq[i-1]);
+        differences.insert(abs(seq[i] - seq[i-1]));
       }
       cin.ignore();
 
-      for (int i=0; i < numbers-1; i++) {
-        not_jolly &= (diffs[i] == 0 || diffs[i] >= numbers);
-        cout << diffs[i] << ' ';
-      }
+      for (int i=0; i < numbers-1; i++)
+        is_jolly &= (diffs[i] >= 1 && diffs[i] <= numbers-1);
 
-      string res = (not_jolly) ? "Not jolly" : "Jolly";
+      for (int i=1; i <= numbers-1; i++)
+        is_jolly = (is_jolly && (differences.find(i) != differences.end()));
+
+      string res = (is_jolly) ? "Jolly" : "Not jolly";
       cout << res << endl;
+      differences.clear();
   }
 
   return 0;
