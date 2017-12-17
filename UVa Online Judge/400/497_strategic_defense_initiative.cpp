affa2a09e551ce82340d66bc8a73709ee1557717
johanjerger <johan@debian>
2017-12-16 22:55:58 -0300

    UVa 497 solved (updated)

--- UVa Online Judge/400/497_strategic_defense_initiative.cpp
+++ UVa Online Judge/400/497_strategic_defense_initiative.cpp
@@ -8,41 +8,42 @@ using namespace std;
 
 int main ()
 {
-  int test_cases, misil;
-  string input;
-  vector<int> sequence;
+        int test_cases, misil, size;
+        string input;
+        vector<int> sequence;
 
-  cin >> test_cases, cin.ignore(), cin.ignore();
-  while (test_cases--)
-  {
-    while (getline(cin, input) && !input.empty()) {
-        stringstream ss(input);
-        ss >> misil;
-        sequence.push_back(misil);
-    }
+        cin >> test_cases, cin.ignore(), cin.ignore();
+        while (test_cases--)
+        {
+                while (getline(cin, input) && !input.empty()) {
+                        stringstream ss(input);
+                        ss >> misil;
+                        sequence.push_back(misil);
+                }
 
-    vector<int> LIS(sequence.size(), 1);
-    vector<int> path(sequence.size(), -1);
-    LIS[0] = 1;
+                size = sequence.size();
+                vector<int> lis(size, 1);
+                vector<int> path(size, -1);
+                lis[0] = 1;
 
-    for (int i = 1; i < sequence.size(); ++i)
-      for (int j = 0; j < i; ++j) {
-          int l = (sequence[j] < sequence[i]) ? LIS[j] + 1 : 0;
-          if (l > LIS[i]) LIS[i] = l, path[i] = static_cast<int>(j);
-      }
+                for (int i = 1; i < size; ++i)
+                        for (int j = 0; j < i; ++j) {
+                                int l = (sequence[j] < sequence[i]) ? lis[j] + 1 : 0;
+                                if (l > lis[i]) lis[i] = l, path[i] = j;
+                        }
 
-    int i = static_cast<int>(distance(LIS.begin(), max_element(LIS.begin(), LIS.end())));
+                int i = distance(lis.begin(), max_element(lis.begin(), lis.end()));
 
-    vector<int> print(1, sequence[i]);
-    while (path[i] != -1) {
-        i = path[i];
-        print.insert(print.begin(), sequence[i]);
-    }
+                vector<int> print(1, sequence[i]);
+                while (path[i] != -1) {
+                        i = path[i];
+                        print.insert(print.begin(), sequence[i]);
+                }
 
-    cout << "Max hits: " << print.size() << endl;
-    copy(print.begin(), print.end(), ostream_iterator<int>(cout, "\n"));
+                cout << "Max hits: " << print.size() << endl;
+                copy(print.begin(), print.end(), ostream_iterator<int>(cout, "\n"));
 
-    if (test_cases) cout << endl;
-    sequence.clear();
-  }
+                if (test_cases) cout << endl;
+                sequence.clear();
+        }
 }
